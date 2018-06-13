af52d85356304663a3e98624b339a5d56325bb68
Socorro Diaz <sodays1@gmail.com>
2018-06-13 20:13:09 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,24 @@
+run test
+def full_name first_name, last_name
+  first_name + "" + last_name
+end
+puts full_name "Socorro", "Diaz"
+def instroduction
+end
+def introduction "Josh" language "English"
+end
+def introduction (Josh)
+end
+def introduction(Josh)
+end
+def introduction "Hi, my name is Josh.\n", language "English"
+end
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+#introduction_with_language
+puts `"Hi, my name is #{name} and I am learning to program in #{language}."
+end
+def introdcution "Hi, my name is Josh #{name} and I am learning to program in #{English}
+end
+run test
