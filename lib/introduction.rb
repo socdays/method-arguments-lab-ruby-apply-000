b70a79fedc020816dc237293d791cb246f3e7039
Socorro Diaz <sodays1@gmail.com>
2018-07-12 19:21:55 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,36 +1,16 @@
-run test
-def full_name first_name, last_name
-  first_name + "" + last_name
+def introduction
 end
-puts full_name "Socorro", "Diaz"
-def instroduction
+def introduction (name)
 end
-def introduction "Josh" language "English"
-end
-def introduction (Josh)
-end
-def introduction(Josh)
-end
-def introduction "Hi, my name is Josh.\n", language "English"
-end
-def introduction(name)
+def introduction (name)
   puts "Hi, my name is #{name}."
 end
-#introduction_with_language
-puts `"Hi, my name is #{name} and I am learning to program in #{language}."
-end
-def introdcution "Hi, my name is Josh #{name} and I am learning to program in #{English}
-end
 run test
-def introduction (Josh)
-  puts "My name is #{Josh}."
+def introduction_with_language
 end
-run tests
-def instroduction "Hi, my name #{Josh}" and I am learning to program in #{English}
-
-  puts "and I am learning to program in #{English}"
+def introduction_with_language (language)
+  puts "Hi, my is #{Josh} and I am learning to program in #{English}."
 end
 run test
 
-  run test
-  run tests
+  
