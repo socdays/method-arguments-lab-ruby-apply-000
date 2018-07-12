2f9f9aa5c8057361a9ecb13adc08d86d11e29812
Socorro Diaz <sodays1@gmail.com>
2018-07-12 21:25:24 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -23,3 +23,11 @@ run test
 #lib.introduction.rb
 def introduction(name)
 end
+puts "Hi, my name is #{Josh}."
+end
+def introduction_with_language
+end
+def introduction_with_language(language)
+end
+puts "and I am learning to program in #{English}."
+end
